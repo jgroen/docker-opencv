1d9cceaa2cf9e6eb7350aa2027c33adf5bd88640
josh.groen@gmail.com <josh.groen@gmail.com>
2016-10-13 19:11:30 -0500

    new file:   chute/run.sh

---
+++ chute/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# You can implement logic here to do periodic network tests and save the
+# results.  You can do that directly from this bash script or call another
+# program that you write (e.g. in Python).
+while true; do
+    sleep 60
+done
+
+# If execution reaches this point, the chute will stop running.
+exit 0
